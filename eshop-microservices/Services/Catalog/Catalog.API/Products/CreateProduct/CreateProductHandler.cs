47fe3072d8d4d557785ec6017b4a1002a8a81847
Ravi Maurya <mauryaravi021@gmail.com>
2025-07-12 01:21:02 +0530

    Implementaion for catalog API

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,31 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create product entity from command object
+            //save to database
+            //return createproductresult result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            // save to database
+
+            //return result
+            return new CreateProductResult(Guid.NewGuid());
+        }
     }
 }
--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreteProductEndpoint.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreteProductEndpoint.cs
@@ -1,6 +1,23 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
-    public class CreteProductEndpoint
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
+    public class CreteProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var commmnd = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(commmnd);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("Create Product")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+        }
     }
 }
