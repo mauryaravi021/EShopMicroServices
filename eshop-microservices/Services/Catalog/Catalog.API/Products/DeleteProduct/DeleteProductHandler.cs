da233a0c7d96bb5c8fbc665d24d784be149ea121
Ravi Maurya <mauryaravi021@gmail.com>
2025-07-12 21:51:49 +0530

    Catalog API is implemented

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using JasperFx.Events.Daemon;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductComamnd(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductHandler> logger) : ICommandHandler<DeleteProductComamnd, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductComamnd command, CancellationToken cancellationToken)
+        {
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    public class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductByCategoryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            var products = await session.Query<Product>()
+           .Where(p => p.Category.Contains(query.Category))
+           .ToListAsync(cancellationToken);
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
